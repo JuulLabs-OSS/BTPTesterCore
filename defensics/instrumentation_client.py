511daa8702a4f6d804a88521e0b6b839c72e3b8e
Michał Narajowski <michal.narajowski@codecoup.pl>
2019-10-15 12:12:19 +0200

    defensics: Add instrumentation client script

---
+++ defensics/instrumentation_client.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python3
+import collections
+import json
+import os
+import sys
+import urllib.error
+import urllib.parse
+import urllib.request
+
+
+# Build a dict with environment variables beginning with CODE_
+def get_code_env():
+    PREFIX = 'CODE_'
+    code_env = [var for var in list(os.environ.keys()) if var.startswith(PREFIX)]
+    values = {var: os.environ[var] for var in code_env}
+    return values
+
+
+# Send all CODE_ environment variables over HTTP; expect JSON back
+def send_http_get_json(uri):
+    data = json.dumps(get_code_env()).encode()
+    req = urllib.request.Request(uri, data, {'Content-Type': 'application/json'})
+    response = urllib.request.urlopen(req)
+    result = response.read()
+    return collections.defaultdict(lambda: None, json.loads(result))
+
+
+def usage():
+    script_name = os.path.basename(sys.argv[0])
+    print(("Usage: {} http://server/(path)".format(script_name)))
+    sys.exit(1)
+
+
+if __name__ == '__main__':
+    rv = 0  # return value
+
+    url = ""
+    if len(sys.argv) > 1:
+        url = sys.argv[1]
+    if not url.startswith('http'):
+        usage()
+
+    # Parse response as JSON and return exit code based on verdict
+    try:
+        data = send_http_get_json(url)
+        print(data)
+        # data = {
+        #    logs: {'file1.log': ['Log line', 'Another line']},
+        #    verdict: 'fail'
+        # }
+        if data['logs']:
+            assert data['logs'].__class__ == dict
+            for name, lines in list(data['logs'].items()):
+                for line in lines:
+                    print(("{}: {}".format(name, line.encode('UTF-8'))))
+        if data['errors']:
+            for err in data['errors']:
+                print("Error: ", err)
+        if data['verdict'] == 'fail':
+            # Retun failure exit code for Defensics; used with "as instrumentation"
+            rv = 1
+    except Exception as e:
+        print("Response not in expected format (JSON dictionary)")
+        print(e)
+        rv = 1
+    finally:
+        sys.exit(rv)
